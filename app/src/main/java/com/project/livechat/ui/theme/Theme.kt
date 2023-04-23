48c460377b0ceda75e9bb4f4dd2d0cd59c359320
Eduardo Santos <edu_felip@hotmail.com>
2023-04-23 18:34:50 -0300

    foundation - removing unnecessary pieces of code

--- app/src/main/java/com/project/livechat/ui/theme/Theme.kt
+++ app/src/main/java/com/project/livechat/ui/theme/Theme.kt
@@ -53,12 +53,6 @@ fun LiveChatTheme(
         else -> LightColorScheme
     }
     val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
-    }
 
     MaterialTheme(
         colorScheme = colorScheme,
